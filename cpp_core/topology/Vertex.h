59a727354d68be9ffadde71dfc31ecdb59226c3d
LEGEND'S DaD <legendsdad001@gmail.com>
2026-03-08 17:32:29 +0530

    Publish

--- cpp_core/topology/Vertex.h
+++ cpp_core/topology/Vertex.h
@@ -1,11 +1,8 @@
 #pragma once
 #include "../math/Vector3.h"
 
-struct HalfEdge;
-
 struct Vertex {
     Vector3 position;
-    HalfEdge* edge = nullptr;
 
     Vertex(const Vector3& pos) : position(pos) {}
 };
